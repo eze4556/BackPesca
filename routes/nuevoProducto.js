baaf8fd748df245b73f5eb8fa20f36134acd718f
eze4556 <ezegonzalez800@gmail.com>
2024-04-07 14:46:03 -0300

    cambios

--- routes/nuevoProducto.js
+++ routes/nuevoProducto.js
@@ -1,7 +1,7 @@
 // routes/nuevoProductoRoutes.js
 const express = require('express');
 const router = express.Router();
-const nuevoProductoController = require('../controllers/nuevoProductoController');
+const nuevoProductoController = require('../controller/nuevoPcontroller');
 
 // Ruta para crear un nuevo producto
 router.post('/nuevo-producto', nuevoProductoController.createNuevoProducto);

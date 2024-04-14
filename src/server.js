bebf228d0a4778c0d3ec332c234a5e5bef7e2de7
eze4556 <ezegonzalez800@gmail.com>
2024-04-14 19:34:01 -0300

    env

--- src/server.js
+++ src/server.js
@@ -18,7 +18,7 @@ const quienesSomos = require('../routes/quienesSomos')
 
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
 // Middlewares
 app.use(cors());
